0ff3c4ed42d36b87b2692c283470fb22e22d0104
deajan <ozy@netpower.fr>
2014-11-24 23:15:32 +0100

    Updated install script with osync-batch.

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-SCRIPT_BUILD=2605201401
+SCRIPT_BUILD=2411201401
 
 ## Osync daemon install script
-## Tested on RHEL / CentOS 6
+## Tested on RHEL / CentOS 6 & 7
 ## Please adapt this to fit your distro needs
 
 mkdir /etc/osync
 cp ./sync.conf /etc/osync/sync.conf.example
 cp ./exclude.list.example /etc/osync
 cp ./osync.sh /usr/local/bin
+cp ./osync-batch.sh /usr/local/bin
 cp ./osync-srv /etc/init.d
 
