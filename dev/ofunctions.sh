1bbd495e2ada1fb6de38e957760c1f54ef4acb1a
deajan <ozy@netpower.fr>
2016-07-19 10:52:43 +0200

    Fix for #52

--- dev/ofunctions.sh
+++ dev/ofunctions.sh
@@ -1,4 +1,4 @@
-## FUNC_BUILD=2016052602
+## FUNC_BUILD=2016071901
 ## BEGIN Generic functions for osync & obackup written in 2013-2016 by Orsiris de Jong - http://www.netpower.fr - ozy@netpower.fr
 
 ## type -p does not work on platforms other than linux (bash). If if does not work, always assume output is not a zero exitcode
@@ -354,7 +354,7 @@ function SendAlert {
 # smtp_server.domain.tld is mandatory, as is smtp_port (should be 25, 465 or 587)
 # encryption can be set to tls, ssl or none
 # smtp_user and smtp_password are optional
-# SendEmail "subject" "Body text" "receiver@example.com receiver2@otherdomain.com" "/path/to/attachment.file" "sender_email@example.com" "smtp_server.domain.tld" "smtp_port" "encrpytion" "smtp_user" "smtp_password"
+# SendEmail "subject" "Body text" "receiver@example.com receiver2@otherdomain.com" "/path/to/attachment.file" "sender_email@example.com" "smtp_server.domain.tld" "smtp_port" "encryption" "smtp_user" "smtp_password"
 function SendEmail {
 	local subject="${1}"
 	local message="${2}"
@@ -363,7 +363,7 @@ function SendEmail {
 	local sender_email="${5}"
 	local smtp_server="${6}"
 	local smtp_port="${7}"
-	local encrpytion="${8}"
+	local encryption="${8}"
 	local smtp_user="${9}"
 	local smtp_password="${10}"
 
@@ -779,7 +779,7 @@ function WaitForCompletion {
 	__CheckArguments 4 $# ${FUNCNAME[0]} "$@"				#__WITH_PARANOIA_DEBUG
 
 	local soft_alert=0 # Does a soft alert need to be triggered, if yes, send an alert once
-	local log_ttime=0 # local time instance for comparaison
+	local log_time=0 # local time instance for comparaison
 
 	local seconds_begin=$SECONDS # Seconds since the beginning of the script
 	local exec_time=0 # Seconds since the beginning of this function
