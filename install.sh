f1c62c03bb34258317cad453b9c617584413e4c1
Shadowigor <shadowigorr@gmail.com>
2016-08-28 18:28:19 +0200

    Executing mkdir for BIN_DIR should be dependent on FAKEROOT for installation

--- install.sh
+++ install.sh
@@ -283,11 +283,7 @@ do
 done
 
 if [ "$FAKEROOT" != "" ]; then
-	mkdir -p $SERVICE_DIR_SYSTEMD_SYSTEM $SERVICE_DIR_SYSTEMD_USER
-fi
-
-if [ "$BIN_DIR" != "/usr/local/bin" ]; then
-	mkdir -p "$BIN_DIR"
+	mkdir -p "$SERVICE_DIR_SYSTEMD_SYSTEM" "$SERVICE_DIR_SYSTEMD_USER" "$BIN_DIR"
 fi
 
 SetOSSettings
