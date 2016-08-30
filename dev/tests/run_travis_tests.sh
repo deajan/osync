939bc45dc719071bee791291fb6f2df484df0a66
deajan <ozy@netpower.fr>
2016-08-30 17:08:00 +0200

    Improved unit tests

--- dev/tests/run_travis_tests.sh
+++ dev/tests/run_travis_tests.sh
@@ -11,8 +11,8 @@ TESTS_DIR="$DEV_DIR/tests"
 
 OSYNC_EXECUTABLE="osync.sh"
 
-INITIATOR_DIR="$TESTS_DIR/opt/osync/initiator"
-TARGET_DIR="$TESTS_DIR/opt/osync/target"
+INITIATOR_DIR="${HOME}/osync/initiator"
+TARGET_DIR="${HOME}/osync/target"
 OSYNC_STATE_DIR=".osync_workdir/state"
 
 function CreateReplicas () {
@@ -29,18 +29,11 @@ function CreateReplicas () {
 
 function oneTimeSetUp () {
 	source "$DEV_DIR/ofunctions.sh"
-
-	if grep "^IS_STABLE=YES" "$DEV_DIR/$OSYNC_EXECUTABLE" > /dev/null; then
-		IS_STABLE=yes
-	else
-		IS_STABLE=no
-		sed -i 's/^IS_STABLE=no/IS_STABLE=yes/' "$DEV_DIR/$OSYNC_EXECUTABLE"
-	fi
 }
 
 function oneTimeTearDown () {
 	if [ "$IS_STABLE" == "no" ]; then
-		sed -i 's/^IS_STABLE=yes/IS_STABLE=no/' "$DEV_DIR/$OSYNC_EXECUTABLE"
+		sed -i.tmp 's/^IS_STABLE=yes/IS_STABLE=no/' "$OSYNC_DIR/$OSYNC_EXECUTABLE"
 	fi
 }
 
@@ -50,10 +43,21 @@ function test_Merge () {
         assertEquals "Merging code" "0" $?
 }
 
+function test_SetStable () {
+	if grep "^IS_STABLE=YES" "$OSYNC_DIR/$OSYNC_EXECUTABLE" > /dev/null; then
+		IS_STABLE=yes
+		echo "Is already set as stable"
+	else
+		IS_STABLE=no
+		sed -i.tmp 's/^IS_STABLE=no/IS_STABLE=yes/' "$OSYNC_DIR/$OSYNC_EXECUTABLE"
+		assertEquals "Set as stable" "0" $?
+	fi
+}
+
 function test_osync_quicksync_local () {
 	CreateReplicas
-	cd "$DEV_DIR"
-	./$OSYNC_EXECUTABLE --initiator="$INITIATOR_DIR" --target="$TARGET_DIR" > /dev/null
+	cd "$OSYNC_DIR"
+	./$OSYNC_EXECUTABLE --initiator="$INITIATOR_DIR" --target="$TARGET_DIR"
 	assertEquals "Return code" "0" $?
 
 	[ -d "$INITIATOR_DIR/$OSYNC_STATE_DIR" ]
