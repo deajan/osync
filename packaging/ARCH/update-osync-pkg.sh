38dc01ee399fcedccb744fd72d0d9b5b733434f0
Shadowigor <shadowigorr@gmail.com>
2016-09-10 12:35:54 +0200

    Removed makepkg dependency

--- packaging/ARCH/update-osync-pkg.sh
+++ packaging/ARCH/update-osync-pkg.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-git clone git+ssh://aur@aur.archlinux.org/osync.git osync.aur
+git clone git+ssh://aur@aur.archlinux.org/osync.git osync.aur &&
 cd "osync.aur" &&
-../makepkg -c &&
-rm -rf osync* &&
+srcdir="." &&
+source "PKGBUILD" &&
+url=$(echo -n ${source[0]} | sed 's/git+//g' | sed 's/#.*//g') &&
+branch=$(echo -n ${source[0]} | sed 's/.*#branch=//g') &&
+git clone -b $branch $url &&
+sed -i "s/pkgver=.*/pkgver=$(pkgver)/g" "PKGBUILD" &&
 ../mksrcinfo &&
+rm -rf "osync" &&
+git add . &&
+git commit -m "Updated version" &&
 git push origin master &&
 cd .. &&
 rm -rf "osync.aur" &&
