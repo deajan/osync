382db0d8043a9c4adebcfb8762ce5f40f7e9173d
deajan <ozy@netpower.fr>
2016-11-12 14:36:59 +0100

    Minor fixes

--- packaging/ARCH/update-osync-pkg.sh
+++ packaging/ARCH/update-osync-pkg.sh
@@ -5,13 +5,15 @@ cd "osync.aur" &&
 srcdir="." &&
 source "PKGBUILD" &&
 
-# Get pkgver from current osync
-pkgver=$(grep PROGRAM_VERSION= ../../../osync.sh)
-pkgver=${pkgver##*=}
-
 url=$(echo -n ${source[0]} | sed 's/git+//g' | sed 's/#.*//g') &&
 branch=$(echo -n ${source[0]} | sed 's/.*#branch=//g') &&
 git clone -b $branch $url &&
+
+# Get pkgver from current osync
+pkgver=$(grep PROGRAM_VERSION= ./osync/osync.sh)
+pkgver=${pkgver##*=}
+echo $pkgver
+
 sed -i "s/pkgver=.*/pkgver=$(pkgver)/g" "PKGBUILD" &&
 ../mksrcinfo &&
 rm -rf "osync" &&
