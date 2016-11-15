d3643d0e9bc4f9ac427fe34281fadb5af22dbbb7
deajan <ozy@netpower.fr>
2016-11-16 00:45:01 +0100

    Local runs should not test connectivity

--- dev/n_osync.sh
+++ dev/n_osync.sh
@@ -4,7 +4,7 @@ PROGRAM="osync" # Rsync based two way sync engine with fault tolerance
 AUTHOR="(C) 2013-2016 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr/osync - ozy@netpower.fr"
 PROGRAM_VERSION=1.1.5
-PROGRAM_BUILD=2016111504
+PROGRAM_BUILD=2016111505
 IS_STABLE=yes
 
 source "./ofunctions.sh"
@@ -1070,8 +1070,6 @@ function Sync {
 	__CheckArguments 0 $# ${FUNCNAME[0]} "$@"	#__WITH_PARANOIA_DEBUG
 
 	Logger "Starting synchronization task." "NOTICE"
-	CheckConnectivity3rdPartyHosts
-	CheckConnectivityRemoteHost
 
 	if [ -f "${INITIATOR[7]}" ] && [ "$RESUME_SYNC" != "no" ]; then
 		resume_sync=$(cat "${INITIATOR[7]}")
