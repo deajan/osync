2fd60182c04ffea38804ac19e058349538672910
deajan <ozy@netpower.fr>
2016-11-22 08:55:51 +0100

    Fixing softdel cleanup test...Again

--- dev/tests/run_tests.sh
+++ dev/tests/run_tests.sh
@@ -526,7 +526,7 @@ function test_softdeletion_cleanup () {
 			[ -f "$file.new" ]
 			assertEquals "New softdeleted / backed up file [$file.new] exists." "0" $?
 
-			if [ "$TRAVIS_RUN" == true ] && [ "$LOCAL_OS" == "BSD" ] && [ "$LOCAL_OS" == "MacOSX" ]; then
+			if [ "$TRAVIS_RUN" == true ] || [ "$LOCAL_OS" == "BSD" ] || [ "$LOCAL_OS" == "MacOSX" ]; then
 				[ ! -f "$file.old" ]
 				assertEquals "Old softdeleted / backed up file [$file.old] is deleted permanently." "0" $?
 			else
