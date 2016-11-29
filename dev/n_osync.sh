be852ca7dff2a0fbe8c652d45c44b66c76497394
deajan <ozy@netpower.fr>
2016-11-29 22:40:43 +0100

    Better time control in sync function

--- dev/n_osync.sh
+++ dev/n_osync.sh
@@ -4,7 +4,7 @@ PROGRAM="osync" # Rsync based two way sync engine with fault tolerance
 AUTHOR="(C) 2013-2016 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr/osync - ozy@netpower.fr"
 PROGRAM_VERSION=1.2-beta3
-PROGRAM_BUILD=2016112901
+PROGRAM_BUILD=2016112902
 IS_STABLE=no
 
 # Execution order						#__WITH_PARANOIA_DEBUG
