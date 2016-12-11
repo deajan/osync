28460a714efa06e115bb71ecc451e4e7f580553d
deajan <ozy@netpower.fr>
2016-12-11 21:57:31 +0100

    Added more check skips

--- dev/shellcheck.sh
+++ dev/shellcheck.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#SC1090 = not following non constants source
+#SC1091 = not following source
+#SC2086 = quoting errors (shellcheck is way too picky about quoting)
 #SC2120 = only for debug version
 
-shellcheck -e SC2086,SC2119,SC2120 $1
+shellcheck -e SC1090,SC1091,SC2086,SC2119,SC2120 $1
