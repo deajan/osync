7039a8a16c5ebf37c2da402dbd6dd8ff71f596f7
deajan <ozy@netpower.fr>
2016-12-13 20:41:25 +0100

    Bumped version to v1.2-RC1

--- dev/n_osync.sh
+++ dev/n_osync.sh
@@ -3,12 +3,9 @@
 PROGRAM="osync" # Rsync based two way sync engine with fault tolerance
 AUTHOR="(C) 2013-2016 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr/osync - ozy@netpower.fr"
-PROGRAM_VERSION=1.2-beta3
+PROGRAM_VERSION=1.2-RC1
 PROGRAM_BUILD=2016121302
-IS_STABLE=no
-
-#TODO(low): is debug subset relevant in remote env
-#TODO(high): check parallel functions for $RUNDIR type output not interferring on both simultaneous runs
+IS_STABLE=yes
 
 # Execution order						#__WITH_PARANOIA_DEBUG
 #	Function Name				Is parallel	#__WITH_PARANOIA_DEBUG
