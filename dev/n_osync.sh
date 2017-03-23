19c15d2265b61ad673ff626103a1c6bd334a7752
deajan <ozy@netpower.fr>
2017-03-23 23:02:43 +0100

    Bumped version to v1.2 final

--- dev/n_osync.sh
+++ dev/n_osync.sh
@@ -3,9 +3,9 @@
 PROGRAM="osync" # Rsync based two way sync engine with fault tolerance
 AUTHOR="(C) 2013-2017 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr/osync - ozy@netpower.fr"
-PROGRAM_VERSION=1.2-RC3
+PROGRAM_VERSION=1.2
 PROGRAM_BUILD=2017032101
-IS_STABLE=no
+IS_STABLE=yes
 
 ##### Execution order						#__WITH_PARANOIA_DEBUG
 #####	Function Name				Is parallel	#__WITH_PARANOIA_DEBUG
