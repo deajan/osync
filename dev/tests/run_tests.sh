704d3d20a73589887af2ac88db81bb59d2ace783
deajan <ozy@netpower.fr>
2018-07-02 15:33:49 +0200

    Travis... travis...travis...

--- dev/tests/run_tests.sh
+++ dev/tests/run_tests.sh
@@ -306,11 +306,11 @@ function test_Merge () {
 	# Don't use SetConfFileValue here since for whatever reason Travis does not like creating a sed temporary file in $FAKEROOT
 
 	if [ "$TRAVIS_RUN" == true ]; then
-		$SUDO_CMD sed -i.tmp 's/IS_STABLE=*/IS_STABLE=yes/' "$OSYNC_EXECUTABLE"
+		$SUDO_CMD sed -i.tmp 's/^IS_STABLE=.*/IS_STABLE=yes/' "$OSYNC_EXECUTABLE"
 	else
-		sed -i.tmp 's/IS_STABLE=*/IS_STABLE=yes/' "$OSYNC_EXECUTABLE"
+		sed -i.tmp 's/^IS_STABLE=.*/IS_STABLE=yes/' "$OSYNC_EXECUTABLE"
 	fi
-	head $OSYNC_EXECUTABLE
+	head -n 20 $OSYNC_EXECUTABLE
 	#SetConfFileValue "$OSYNC_EXECUTABLE" "IS_STABLE" "yes"
 
 
