3edd80949fb1f7a24955181aeaa35f532de87b0e
deajan <ozy@netpower.fr>
2019-05-20 23:31:04 +0200

    Allow commandline override of config file values

--- dev/n_osync.sh
+++ dev/n_osync.sh
@@ -7,7 +7,7 @@ PROGRAM="osync" # Rsync based two way sync engine with fault tolerance
 AUTHOR="(C) 2013-2019 by Orsiris de Jong"
 CONTACT="http://www.netpower.fr/osync - ozy@netpower.fr"
 PROGRAM_VERSION=1.3.0-beta2
-PROGRAM_BUILD=2019052015
+PROGRAM_BUILD=2019052016
 IS_STABLE=false
 
 CONFIG_FILE_REVISION_REQUIRED=1.3.0
@@ -3167,6 +3167,9 @@ else
 	Logger "Wrong arguments given. Expecting a config file or initiator and target arguments." "CRITICAL"
 	exit 1
 fi
+
+# Reload GetCommandLineArguments so we can override config file with run time arguments
+GetCommandlineArguments "${@}"
 
 if [ "$LOGFILE" == "" ]; then
 	if [ -w /var/log ]; then
