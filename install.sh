72f2e4307133d3c990a6bfc43637099ad181c196
deajan <ozy@netpower.fr>
2019-05-22 19:43:48 +0200

    Rebuilt targets

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ INSTANCE_ID="installer-$SCRIPT_BUILD"
 ## Please adapt this to fit your distro needs
 
 _OFUNCTIONS_VERSION=2.3.0-dev-postRC2
-_OFUNCTIONS_BUILD=2019052103
+_OFUNCTIONS_BUILD=2019052201
 _OFUNCTIONS_BOOTSTRAP=true
 
 if ! type "$BASH" > /dev/null; then
